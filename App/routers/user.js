5e6758695883c019b93b7807296a0e2e31e7d4a8
shoelfikar <sulfikardi25@gmail.com>
2020-09-11 16:57:24 +0700

    edit header

--- App/routers/user.js
+++ App/routers/user.js
@@ -8,7 +8,7 @@ const auth = require('../middleware/auth')
 Router
     .post('/signup', userController.signUp)
     .post('/signin', userController.signIn)
-    .get('/', userController.getAllUsers)
+    .get('/',auth.verifyToken, userController.getAllUsers)
 
 
 
